f2ee09b37db58f1a933c9a0f23d06a6b63a9b1c9
Rickard Bremer <rickard.bremer@gmail.com>
2020-04-17 18:30:13 +0200

    Added some functions

--- telegram_bot_firewall.py
+++ telegram_bot_firewall.py
@@ -1,28 +1,32 @@
-import re
 import subprocess
+from github import Github
+from datetime import datetime, timedelta
 from telegram.ext import Updater, CommandHandler
-import requests
 
 
-def get_url():
-    contents = requests.get('https://random.dog/woof.json').json() 
-    url = contents['url']
-    return url
+def project(bot, update):
+    update.message.reply_text("Redmine : http://64.227.73.149/redmine/\nGithub: https://github.com/alpha031/firewall/")
+
+def latest_commit(bot, update):
+    with open('git_token') as file:
+        import pdb
+        pdb.set_trace()
+        git_token = file.read()
+        git_token = git_token.rstrip('\n')
+        access = Github(git_token)
+        org = access.get_organization('alpha031')
+        code = org.get_repo('firewall')
+        since = datetime.now() - timedelta(days=1)
+        commits = code.get_commits(since=since)
+        update.message.reply_text(commits[0])
 
 
-def get_image_url():
-    allowed_extension = ['jpg', 'jpeg', 'png']
-    file_extension = ''
-    while file_extension not in allowed_extension:
-        url = get_url()
-        file_extension = re.search("([^.]*)$", url).group(1).lower()
-    return url
-
-
-def bop(bot, update):
-    url = get_image_url()
-    chat_id = update.message.chat_id
-    bot.send_photo(chat_id=chat_id, photo=url)
+def help_commands(bot, update):
+    help_commands = "Commands available : "
+    commands = ["help", "fortune", "latest_commit", "project"]
+    for item in commands:
+        help_commands += item + " : "     
+    update.message.reply_text(help_commands)
 
 
 def fortune(bot, update):
@@ -31,13 +35,15 @@ def fortune(bot, update):
 
 
 def main():
-    with open('token') as file:
-        token = file.read()
-        token = token.rstrip('\n')
-    updater = Updater(token)
+    with open('telegram_token') as file:
+        telegram_token = file.read()
+        telegram_token = telegram_token.rstrip('\n')
+    updater = Updater(telegram_token)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('bop', bop))
     dp.add_handler(CommandHandler('fortune', fortune))
+    dp.add_handler(CommandHandler('latest_commit', latest_commit))
+    dp.add_handler(CommandHandler('help', help_commands))
+    dp.add_handler(CommandHandler('project', project))
     updater.start_polling()
     updater.idle()
 
